1f69cf0ba6033ade31122ed407f20cd17c26fa81
moon <moon@Moon-PC>
2018-06-06 21:13:09 -0700

    assert

--- testGit/src/testGit/Test_1.java
+++ testGit/src/testGit/Test_1.java
@@ -1,5 +1,6 @@
 package testGit;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,14 +9,17 @@ import org.openqa.selenium.interactions.Actions;
 
 public class Test_1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\moon\\Downloads\\Chrome\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		Thread.sleep(3000L);
+		
 		driver.get("https://www.amazon.com/");
 		
 		System.out.println("Test.java runs");
+		Assert.assertEquals(driver.getTitle(), "amazone");
 		//Actions a = new Actions(driver);
 		//WebElement move= driver.findElement(By.id("twotabsearchtextbox"));
 	    //move.sendKeys("Hello");
