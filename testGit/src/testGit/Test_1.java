a1c17423de9614348fa44a6fd3f790267b4f3812
moon <moon@Moon-PC>
2018-06-09 22:35:06 -0700

    update TestNG

--- testGit/src/testGit/Test_1.java
+++ testGit/src/testGit/Test_1.java
@@ -6,24 +6,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
 
 public class Test_1 {
 
-	public static void main(String[] args) throws InterruptedException {
+	@Test
+	public void testjava()
+	{
+	
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\moon\\Downloads\\Chrome\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		Thread.sleep(3000L);
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\moon\\Downloads\\Chrome\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.amazon.com/");
+		
+		//driver.get("https://www.amazon.com/");
 		
 		System.out.println("Test.java runs");
-		Assert.assertEquals(driver.getTitle(), "Amazone");
+		//Assert.assertEquals(driver.getTitle(), "Amazone");
 		//Actions a = new Actions(driver);
 		//WebElement move= driver.findElement(By.id("twotabsearchtextbox"));
 	    //move.sendKeys("Hello");
 		
+		
+		
 	}
+		
+	@Test
+	public void test2java()
+	{
+		System.out.println("Testjava 2 runs");
+	}
+	
 
 }
